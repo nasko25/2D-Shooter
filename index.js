d55caf2d5493be417469f454b1b6c044c730c3ab
nasko25 <apashov0@gmail.com>
2019-12-23 15:19:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs")
+
+http.createServer(function(req, res){
+	fs.readFile("index.html", function(err, data){
+		res.writeHead(200, {"Content-Type":"text/html"});
+		res.write(data);
+		var q = url.parse(req.url, true).query;
+		var txt = q.arg1 + " " + q.arg2;
+		res.write("hello <br>");
+		res.write(req.url + "<br><br>");
+		res.end(txt);
+	});
+}).listen(8080);
