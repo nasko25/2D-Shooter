ddd3f78ccfc05a32a8fb87ef8e768b1fab2f0bf8
nasko25 <apashov0@gmail.com>
2019-12-23 23:35:00 +0100

    added a button and serve a static page

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ var url = require("url");
 var fs = require("fs")
 
 http.createServer(function(req, res){
-	fs.readFile("index.html", function(err, data){
-		res.writeHead(200, {"Content-Type":"text/html"});
-		res.write(data);
-		var q = url.parse(req.url, true).query;
-		var txt = q.arg1 + " " + q.arg2;
-		res.write("hello <br>");
-		res.write(req.url + "<br><br>");
-		res.end(txt);
-	});
+
+	if (req.url === "/main.css") {
+		fs.readFile("main.css", function(err, data){
+			res.writeHead(200, {"Content-Type":"text/css"});
+			res.write(data);
+			res.end();
+		});
+	}
+
+	else {
+		fs.readFile("index.html", function(err, data){
+			res.writeHead(200, {"Content-Type":"text/html"});
+			res.write(data);
+			var q = url.parse(req.url, true).query;
+			var txt = q.arg1 + " " + q.arg2;
+			res.write("<br><br> hello <br>");
+			res.write(req.url + "<br><br>");
+			res.end(txt);
+		});
+	}
 }).listen(8080);
