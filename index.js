7310e917a2ba0da3154691674fda3bb9d71db186
nasko25 <apashov0@gmail.com>
2019-12-27 13:02:07 +0100

    added 3 columns for statistics to the home page

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ http.createServer(function(req, res){
 		fs.readFile("index.html", function(err, data){
 			res.writeHead(200, {"Content-Type":"text/html"});
 			res.write(data);
-			var q = url.parse(req.url, true).query;
-			var txt = q.arg1 + " " + q.arg2;
-			res.write("<br><br> hello <br>");
-			res.write(req.url + "<br><br>");
-			res.end(txt);
+			// var q = url.parse(req.url, true).query;
+			// var txt = q.arg1 + " " + q.arg2;
+			// res.write("<br><br> hello <br>");
+			// res.write(req.url + "<br><br>");
+			// res.end(txt);
+			res.end();
 		});
 	}
 }).listen(8080);
