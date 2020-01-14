ac2f918484c5cd7501ffbd09e7bb4a504817fbe2
Faustify <nickyblagoev@gmail.com>
2020-01-14 18:21:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -20,27 +20,50 @@ console.log("Server listening on port " + PORT);
 function Player(id) {
   this.id = id;
   this.rotation = Math.atan2(1, 1);
-  this.x = 10;
-  this.y = 10;
+  this.x = Math.floor((Math.random() * 500) + 10);
+  this.y = Math.floor((Math.random() * 500) + 10);
   this.hit = false;
   this.reload = 0;
   this.xSpeed = 0;
+  this.respawn=0;
+  this.dead=false;
   this.ySpeed = 0;
 	this.maxSpeed = 10;
   this.facing = 0;
+  this.face = function() {
+    if (this.ySpeed > 0 && this.xSpeed > 0) {
+      this.facing = Math.PI * 1 / 4;
+    } else if (this.ySpeed > 0 && this.xSpeed < 0) {
+      this.facing = Math.PI * 3 / 4;
+    } else if (this.ySpeed > 0) {
+      this.facing = Math.PI / 2;
+    } else if (this.xSpeed > 0 && this.ySpeed === 0) {
+      this.facing = 0;
+    } else if (this.ySpeed < 0 && this.xSpeed > 0) {
+      this.facing = -Math.PI * 1 / 4;
+    } else if (this.ySpeed < 0 && this.xSpeed < 0) {
+      this.facing = -Math.PI * 3 / 4;
+    } else if (this.ySpeed < 0) {
+      this.facing = -Math.PI * 1 / 2;
+    } else if (this.xSpeed < 0) {
+      this.facing = -Math.PI;
+    }
+  }
 };
 
 // Bullet
 function Bullet(whoShot) {
   this.x = 0;
   this.y = 0;
+  this.originY=0;
   this.rotation = 0;
   this.speedX = 0;
   this.speedY = 0;
   this.playerId = whoShot;
   this.generate = function(playerX, playerY, velocityX, velocityY, rotation) {
     this.x = playerX;
-    this.y = playerY + 50;
+	this.y = playerY + 50;
+	this.originY=playerY;
     this.rotation = rotation;
   }
   this.move = function() {
@@ -50,6 +73,8 @@ function Bullet(whoShot) {
 
 function Game(p1, p1_websocket, game_id) {
 	this.p1 = p1;
+	
+	
 	this.p1_websocket = p1_websocket;
 	this.p2 = undefined;
 	this.p2_websocket = undefined;
@@ -79,7 +104,9 @@ wss.on("connection", function(ws) {
 		for (var game in games) {
 			// any game waits for players
 			if (games[game].waiting_for_players) {
+				p1.x=Math.floor((Math.random() * 500) + 600);
 				games[game].p2 = p1;
+
 				games[game].p2_websocket = ws;
 				games[game].waiting_for_players = false;
 				game_id = game;
@@ -143,7 +170,7 @@ wss.on("connection", function(ws) {
 		    var bullet = new Bullet(p1.id);
 		    bullet.generate(p1.x + 60, p1.y + 60, -dx * 20 / hyp, -dy * 20 / hyp, p1.rotation);
 		    games[game_id].bullets.push(bullet);
-		    p1.reload = 80;
+		    p1.reload = 400;
 		  }
 		}
 	});
@@ -154,30 +181,151 @@ wss.on("connection", function(ws) {
 		// 		websockets[websock].send(JSON.stringify(["move", {player: p }]));
 		// 	}
 		// }
-		if (p1.ySpeed === 0) {
-	    p1.x = p1.x + p1.xSpeed * 2*4/30;
-	  } else {
-	    p1.x = p1.x + p1.xSpeed * Math.sqrt(50)*4/150;
-	  }
-	  if (p1.xSpeed === 0) {
-	    p1.y = p1.y + p1.ySpeed * 2 *4/30;
-	  } else {
-	    p1.y = p1.y + p1.ySpeed * Math.sqrt(50)*4/150;
-	  }
+		var dist=40;
+		var sphere1X,sphere1Y, sphere2X, sphere2Y;
+		var circ1X, circ1Y, circ2X, circ2Y;
+		if(games[game_id].p2!=undefined){
+		/*if(games[game_id].p1.facing*180/Math.PI===-180){
+			sphere1X=games[game_id].p1.x-dist;
+			sphere1Y=games[game_id].p1.y;
+		}else if(games[game_id].p1.facing*180/Math.PI===-90){
+			sphere1Y=games[game_id].p1.y-dist;
+			sphere1X=games[game_id].p1.x;
+		}else if(games[game_id].p1.facing*180/Math.PI===0){
+			sphere1X=games[game_id].p1.x+dist;
+			sphere1Y=games[game_id].p1.y;
+		}else if(games[game_id].p1.facing*180/Math.PI===90){
+			sphere1Y=games[game_id].p1.y+dist;
+			sphere1X=games[game_id].p1.x;
+		}else if(games[game_id].p1.facing*180/Math.PI===45){
+			sphere1Y=games[game_id].p1.y+dist*Math.sqrt(2)/2;
+			sphere1X=games[game_id].p1.x+dist*Math.sqrt(2)/2;
+		}else if(games[game_id].p1.facing*180/Math.PI===-45){
+			sphere1Y=games[game_id].p1.y-dist*Math.sqrt(2)/2;
+			sphere1X=games[game_id].p1.x+dist*Math.sqrt(2)/2;
+		}else if(games[game_id].p1.facing*180/Math.PI===135){
+			sphere1Y=games[game_id].p1.y+dist*Math.sqrt(2)/2;
+			sphere1X=games[game_id].p1.x-dist*Math.sqrt(2)/2;
+		}else if(games[game_id].p1.facing*180/Math.PI===-135){
+			sphere1Y=games[game_id].p1.y-dist*Math.sqrt(2)/2;
+			sphere1X=games[game_id].p1.x-dist*Math.sqrt(2)/2;
+		}
+		if(games[game_id].p2.facing*180/Math.PI===-180){
+			sphere2X=games[game_id].p2.x-dist;
+			sphere2Y=games[game_id].p2.y;
+		}else if(games[game_id].p2.facing*180/Math.PI===-90){
+			sphere2Y=games[game_id].p2.y-dist;
+			sphere2X=games[game_id].p2.x;
+		}else if(games[game_id].p2.facing*180/Math.PI===0){
+			sphere2X=games[game_id].p2.x+dist;
+			sphere2Y=games[game_id].p2.y;
+		}else if(games[game_id].p2.facing*180/Math.PI===90){
+			sphere2Y=games[game_id].p2.y+dist;
+			sphere2X=games[game_id].p2.x;
+		}else if(games[game_id].p2.facing*180/Math.PI===45){
+			sphere2Y=games[game_id].p2.y+dist*Math.sqrt(2)/2;
+			sphere2X=games[game_id].p2.x+dist*Math.sqrt(2)/2;
+		}else if(games[game_id].p2.facing*180/Math.PI===-45){
+			sphere2Y=games[game_id].p2.y-dist*Math.sqrt(2)/2;
+			sphere2X=games[game_id].p2.x+dist*Math.sqrt(2)/2;
+		}else if(games[game_id].p2.facing*180/Math.PI===135){
+			sphere2Y=games[game_id].p2.y+dist*Math.sqrt(2)/2;
+			sphere2X=games[game_id].p2.x-dist*Math.sqrt(2)/2;
+		}else if(games[game_id].p2.facing*180/Math.PI===-135){
+			sphere2Y=games[game_id].p2.y-dist*Math.sqrt(2)/2;
+			sphere2X=games[game_id].p2.x-dist*Math.sqrt(2)/2;
+		}*/
+			sphere2X=games[game_id].p2.x+41*Math.cos(games[game_id].p2.facing);
+			sphere2Y=games[game_id].p2.y+41*Math.sin(games[game_id].p2.facing);
+			sphere1X=games[game_id].p1.x+41*Math.cos(games[game_id].p1.facing);
+			sphere1Y=games[game_id].p1.y+41*Math.sin(games[game_id].p1.facing);
+			
+			circ2X=games[game_id].p2.x-20*Math.cos(games[game_id].p2.facing);
+			circ2Y=games[game_id].p2.y-20*Math.sin(games[game_id].p2.facing);
+			circ1X=games[game_id].p1.x-20*Math.cos(games[game_id].p1.facing);
+			circ1Y=games[game_id].p1.y-20*Math.sin(games[game_id].p1.facing);
+			
+		if(Math.sqrt((sphere2Y-sphere1Y)*(sphere2Y-sphere1Y)+(sphere1X-sphere2X)*(sphere1X-sphere2X))<78){
+			games[game_id].p2.y=games[game_id].p2.y-games[game_id].p2.ySpeed*16/30;
+			games[game_id].p2.x=games[game_id].p2.x-games[game_id].p2.xSpeed*16/30;
+			games[game_id].p1.y=games[game_id].p1.y-games[game_id].p1.ySpeed*16/30;
+			games[game_id].p1.x=games[game_id].p1.x-games[game_id].p1.xSpeed*16/30;
+			//console.log("collision "+sphere2X+" "+sphere1X+" "+Math.sqrt((sphere2Y-sphere1Y)*(sphere2Y-sphere1Y)+(sphere1X-sphere2X)*(sphere1X-sphere2X)));
+		}else if(Math.sqrt((sphere2Y-circ1Y)*(sphere2Y-circ1Y)+(circ1X-sphere2X)*(circ1X-sphere2X))<78){
+			games[game_id].p2.y=games[game_id].p2.y-games[game_id].p2.ySpeed*16/30;
+			games[game_id].p2.x=games[game_id].p2.x-games[game_id].p2.xSpeed*16/30;
+			games[game_id].p1.y=games[game_id].p1.y-games[game_id].p1.ySpeed*16/30;
+			games[game_id].p1.x=games[game_id].p1.x-games[game_id].p1.xSpeed*16/30;
+			//console.log("collision "+sphere2X+" circ "+sphere1X+" "+Math.sqrt((sphere2Y-sphere1Y)*(sphere2Y-sphere1Y)+(sphere1X-sphere2X)*(sphere1X-sphere2X)));
+		}else if(Math.sqrt((circ2Y-sphere1Y)*(circ2Y-sphere1Y)+(sphere1X-circ2X)*(sphere1X-circ2X))<78){
+			games[game_id].p2.y=games[game_id].p2.y-games[game_id].p2.ySpeed*16/30;
+			games[game_id].p2.x=games[game_id].p2.x-games[game_id].p2.xSpeed*16/30;
+			games[game_id].p1.y=games[game_id].p1.y-games[game_id].p1.ySpeed*16/30;
+			games[game_id].p1.x=games[game_id].p1.x-games[game_id].p1.xSpeed*16/30;
+			//console.log("collision "+sphere2X+" "+sphere1X+" "+Math.sqrt((sphere2Y-sphere1Y)*(sphere2Y-sphere1Y)+(sphere1X-sphere2X)*(sphere1X-sphere2X)));
+		}
+	}
+	/*
+	if(games[game_id].p2!=undefined){
+		if(Math.sqrt((games[game_id].p1.x-games[game_id].p2.x)*(games[game_id].p1.x-games[game_id].p2.x)+(games[game_id].p1.y-games[game_id].p2.y)*(games[game_id].p1.y-games[game_id].p2.y))<100){
+			console.log(games[game_id].p1.x+" x "+games[game_id].p2.x);
+		}
+		
 
+	}
+	*/
+		p1.face();
+		circ2X=p1.x+41*Math.cos(p1.facing);
+		circ2Y=p1.y+41*Math.sin(p1.facing);
+		circ1X=p1.x-20*Math.cos(p1.facing);
+		circ1Y=p1.y-20*Math.sin(p1.facing);
+	
+		if (p1.ySpeed === 0) {
+		p1.x = Math.max(p1.x + p1.xSpeed * 2*4/30,-10);
+		p1.x=Math.min(p1.x,3000);
+	  } else {
+		p1.x = Math.max(p1.x + p1.xSpeed * Math.sqrt(50)*4/150,-10);
+		p1.x=Math.min(p1.x,3000);
+	  }
+	
+	
+	  if (p1.xSpeed === 0) {
+		p1.y = Math.max(p1.y + p1.ySpeed * 2 *4/30,-10);
+		p1.y=Math.min(p1.y,3000);
+	  } else {
+		p1.y =Math.max( p1.y + p1.ySpeed * Math.sqrt(50)*4/150,-10);
+		p1.y=Math.min(p1.y,3000);
+	  }
+	
 		if ((p1.ySpeed !== 0 || p1.xSpeed !== 0)) {
 			games[game_id].p1_websocket.send(JSON.stringify(["move", {x:p1.x, y: p1.y, id: p1.id, xSpeed: p1.xSpeed, ySpeed: p1.ySpeed}]));
 			games[game_id].p2_websocket.send(JSON.stringify(["move", {x:p1.x, y: p1.y, id: p1.id, xSpeed: p1.xSpeed, ySpeed: p1.ySpeed}]));
 		}
-
+		p1.reload = Math.max(p1.reload - 4, 0);
 		games[game_id].bullets.forEach(function(element, index, object) {
 			element.move();
+			//console.log(element.x+" "+element.y+" "+circ2X+" "+circ2Y);
+			var buly,bulx,bulHyp;
+			bulHyp=element.y-element.originY;
+			buly=element.originY+bulHyp*Math.sin(element.rotation)-60;
+			bulx=element.x+bulHyp*Math.cos(element.rotation)-60;
+			//console.log(buly+" "+bulx+" "+p1.x+" "+p1.y+" "+bulHyp);
+			if(element.playerId!=p1.id&&((Math.sqrt((buly-circ1Y)*(buly-circ1Y)+(bulx-circ1X)*(bulx-circ1X))<=46)||(Math.sqrt((buly-circ2Y)*(buly-circ2Y)+(bulx-circ2X)*(bulx-circ2X))<=47))){
+				console.log("shot" + buly+" "+bulx+" "+circ2Y+" "+circ2X+" "+circ1Y+" "+circ1X);
+				object.splice(index, 1);
+				/*
+					ADD YOU GOT SHOT MESSAGE
+				*/
+				games[game_id].p1_websocket.send(JSON.stringify(["dead", {dead:true, respawn: 200, id: p1.id, reload: p1.reload}]));
+				games[game_id].p2_websocket.send(JSON.stringify(["dead", {dead:true, respawn: 200, id: p1.id, reload: p1.reload}]));
+			}else{
 			games[game_id].p1_websocket.send(JSON.stringify(["shoot", {id: element.playerId, x:element.x, y: element.y, reload: p1.reload, rotation: element.rotation}]));
 			games[game_id].p2_websocket.send(JSON.stringify(["shoot", {id: element.playerId, x:element.x, y: element.y, reload: p1.reload, rotation: element.rotation}]));
 		  if (element.y < -500 || element.y > 5000) {
 		    object.splice(index, 1);
 		  }
-			p1.reload = Math.max(p1.reload - 1, 0);
+		}
+			
 		});
 	}, 20);
 	ws.on("close", () => {
