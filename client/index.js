ea0e15fbffdbbec516bb9915393fab3c0c3dab08
Faustify <nickyblagoev@gmail.com>
2020-01-14 18:21:52 +0100

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -18,6 +18,14 @@ var shell = document.getElementById("shell");
 var f1 = document.getElementById("flash1");
 var f2 = document.getElementById("flash2");
 var f3 = document.getElementById("flash3");
+var ex1 = document.getElementById("ex1");
+var ex2 = document.getElementById("ex2");
+var ex3 = document.getElementById("ex3");
+var ex4 = document.getElementById("ex4");
+var ex5 = document.getElementById("ex5");
+var ex6 = document.getElementById("ex6");
+var ex7 = document.getElementById("ex7");
+var ex8 = document.getElementById("ex8");
 var bullets = [];
 
 //Player object
@@ -134,8 +142,8 @@ window.addEventListener('mousemove', (ev) => {
   if (p1) {
     mouse[0] = ev.clientX + this.pageXOffset;
     mouse[1] = ev.clientY + this.pageYOffset;
-    var dx = mouse[0] - point[0] - 9 - (window.innerWidth - canvas.width)/2,
-      dy = mouse[1] - point[1] - 9,
+    var dx = mouse[0] - point[0] -canvas.offsetLeft-3,
+      dy = mouse[1] - point[1] -canvas.offsetTop-3,
       rot = Math.atan2(dy, dx);
     clientObject.rotate=rot;
     // p1.setrotation(rot);
@@ -177,6 +185,9 @@ window.addEventListener("keyup", (e) => {
   } else if (e.code === 'KeyA') {
     clientObject.speedX=0;
     socket.send(JSON.stringify(["key_up", "left"]));
+  }else if (e.code === 'KeyP') {
+    p1.dead=true;
+    p1.respawn=80;
   }
 });
 
@@ -238,6 +249,17 @@ socket.onmessage = function(data) {
     else if (message[1].id === enemy.id) {
       enemy.reload = message[1].reload;
     }
+  } else if (message[0] === "dead") {
+    if (message[1].id === p1.id) {
+      p1.dead=true;
+      p1.respawn=200;
+      p1.reload=message[1].reload;
+    }
+    else if (message[1].id === enemy.id) {
+      enemy.dead=true;
+      enemy.respawn=200;
+      enemy.reload=message[1].reload;
+    }
   }
 
   
@@ -252,6 +274,7 @@ socket.onclose = function() {
 
 var updated = false;
  var speed = setInterval(() => {
+   if(!p1.dead){
    if (clientObject.speedY === 0) {
     clientObject.x = clientObject.x + clientObject.speedX * 2 / 6;
  } else {
@@ -262,6 +285,7 @@ var updated = false;
  } else {
   clientObject.y = clientObject.y + clientObject.speedY * Math.sqrt(50) / 30;
    }
+  }
    if(updated){
    //Check if client differs from server:
    if(Math.abs(clientObject.x-p1.x)>3){
@@ -287,6 +311,7 @@ var updated = false;
 
 // TODO function too big; extract smaller functions and call only them, not calling render() each time
 function render(ctx, width, height) {
+ 
   ctx.clearRect(0, 0, width, height);
   let clX=clientObject.x,clY=clientObject.y, clSpeedX=clientObject.speedX, clSpeedY=clientObject.speedY, clfacing = 0, clRotation=clientObject.rotate;
   // draw the background
@@ -296,12 +321,13 @@ function render(ctx, width, height) {
   ctx.drawImage(backgroundView, -clX, -clY);
 
   // draw the player
+  if(!p1.dead||p1.respawn>170){
   ctx.save();
   ctx.translate(x, y);
   var index = 0;
 
   ctx.rotate(p1.facing + 90 * Math.PI / 180);
-
+ //console.log(clY+" "+(clY+40*Math.max(Math.sin(p1.facing),-10))+" "+(clX+40*Math.max(Math.cos(p1.facing),-10)));
   if (clSpeedX !== 0 || clSpeedY !== 0) {
 
     ctx.drawImage(tracksImg, -65, 20, tracksImg.width * 2 / 3, tracksImg.height * 2 / 3);
@@ -309,11 +335,14 @@ function render(ctx, width, height) {
   }
   ctx.drawImage(hull, -85, -97, hull.width * 2 / 3, hull.height * 2 / 3);
 
+  
+  
   ctx.restore();
-
+  }
   // draw the enemy
   ctx.save();
   ctx.translate(enemy.x - clX + width/2 + 150, enemy.y-clY + height/2 + 100);
+  if(!enemy.dead||enemy.respawn>170){
   ctx.rotate(enemy.facing + 90 * Math.PI / 180);
   if (enemy.xSpeed !== 0 || enemy.ySpeed !== 0) {
 
@@ -321,13 +350,40 @@ function render(ctx, width, height) {
     ctx.drawImage(tracksImg, -15, 20, tracksImg.width * 2 / 3, tracksImg.height * 2 / 3);
   }
   ctx.drawImage(hull, -85, -97, hull.width * 2 / 3, hull.height * 2 / 3);
+}
   ctx.restore();
-
+  
   // draw the enemy's gun
   ctx.save();
   ctx.translate(enemy.x - clX + width/2 + 150, enemy.y-clY + height/2 + 100);
+   if(!enemy.dead||enemy.respawn>170){
   ctx.rotate(enemy.rotation + 90 * Math.PI / 180);
   ctx.drawImage(gun, -20, -73, gun.width * 2 / 3, gun.height * 2 / 3);
+   }
+   if(enemy.dead){
+    var exx=-130,exy=-140;
+    if(enemy.respawn<=200&&enemy.respawn>190){
+      ctx.drawImage(ex1,exx,exy);
+    }else if(enemy.respawn<=190&&enemy.respawn>180){
+      ctx.drawImage(ex2,exx,exy);
+    }else if(enemy.respawn<=180&&enemy.respawn>170){
+      ctx.drawImage(ex3,exx,exy);
+    }else if(enemy.respawn<=170&&enemy.respawn>160){
+      ctx.drawImage(ex4,exx,exy);
+    }else if(enemy.respawn<=160&&enemy.respawn>150){
+      ctx.drawImage(ex5,exx,exy);
+    }else if(enemy.respawn<=150&&enemy.respawn>140){
+      ctx.drawImage(ex6,exx,exy);
+    }else if(enemy.respawn<=140&&enemy.respawn>130){
+      ctx.drawImage(ex7,exx,exy);
+    }else if(enemy.respawn<=130&&enemy.respawn>120){
+      ctx.drawImage(ex8,exx,exy);
+    }
+    enemy.respawn-=5;
+    if(enemy.respawn<1){
+      enemy.dead=false;
+    }
+  }
   ctx.restore();
 
   // draw the bullets
@@ -350,12 +406,16 @@ function render(ctx, width, height) {
     ctx.save();
     if (bullets[index].id === p1.id) {
       ctx.rotate(bullets[index].rotation + 90 * Math.PI / 180);
+      ctx.beginPath();
+      ctx.arc(+8, clY - bullets[index].y+40, 7, 0, 2 * Math.PI);
+       ctx.stroke();
       ctx.drawImage(shell, -35, clY - bullets[index].y, shell.width * 2 / 3, shell.height * 2 / 3);
     }
     else if (bullets[index].id === enemy.id) {
       ctx.translate(enemy.x - clX, enemy.y-clY);
 
       ctx.rotate(bullets[index].rotation + 90 * Math.PI / 180);
+      
       ctx.drawImage(shell, -35, enemy.y - bullets[index].y, shell.width * 2 / 3, shell.height * 2 / 3);
 
       if (enemy.reload <= 80 && enemy.reload > 78) {
@@ -373,22 +433,48 @@ function render(ctx, width, height) {
     ctx.restore();
     bullets.splice(index, 1);
   }
-
+  if(!p1.dead||p1.respawn>170){
   // rotate the gun
   ctx.rotate(clRotation + 90 * Math.PI / 180);
 
   // draw the shooting animation
-  if (p1.reload <= 80 && p1.reload > 78) {
+  if (p1.reload <= 400 && p1.reload > 388) {
+    
     ctx.drawImage(flash1, -60, -200);
-  } else if (p1.reload <= 78 && p1.reload > 74) {
+  } else if (p1.reload <= 388 && p1.reload > 360) {
     ctx.drawImage(flash2, -60, -200);
-  } else if (p1.reload <= 74 && p1.reload > 70) {
+  } else if (p1.reload <= 360 && p1.reload > 340) {
     ctx.drawImage(flash3, -60, -200);
   }
 
   // draw the player's gun
   ctx.drawImage(gun, -20, -73, gun.width * 2 / 3, gun.height * 2 / 3);
-
+  }
+  if(p1.dead){
+    var exx=-130,exy=-140;
+    if(p1.respawn<=200&&p1.respawn>190){
+      ctx.drawImage(ex1,exx,exy);
+    }else if(p1.respawn<=190&&p1.respawn>180){
+      ctx.drawImage(ex2,exx,exy);
+    }else if(p1.respawn<=180&&p1.respawn>170){
+      ctx.drawImage(ex3,exx,exy);
+    }else if(p1.respawn<=170&&p1.respawn>160){
+      ctx.drawImage(ex4,exx,exy);
+    }else if(p1.respawn<=160&&p1.respawn>150){
+      ctx.drawImage(ex5,exx,exy);
+    }else if(p1.respawn<=150&&p1.respawn>140){
+      ctx.drawImage(ex6,exx,exy);
+    }else if(p1.respawn<=140&&p1.respawn>130){
+      ctx.drawImage(ex7,exx,exy);
+    }else if(p1.respawn<=130&&p1.respawn>120){
+      ctx.drawImage(ex8,exx,exy);
+    }
+    p1.respawn-=5;
+    if(p1.respawn<1){
+      p1.dead=false;
+    }
+  }
   ctx.restore();
   //p1.reload = Math.max(p1.reload - 1, 0);
 }
+
